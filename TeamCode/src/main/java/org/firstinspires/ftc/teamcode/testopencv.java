2751cb247cde1e07de6f7644eb385aaaeeb5d2a5
suitbot <suitbots@gmail.com>
2021-11-18 17:44:59 -0800

    Ed - Setting up opencv (in commenrts)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testopencv.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+/*
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+*/
+public class testopencv {/*extends OpenCVPipeline {
+    Telemetry telemetry;
+    Mat mat = new Mat();
+    static final Rect LEFT_ROI = new Rect(
+            new Point(x:60, y:35),
+            new Point(x:120, y:75));
+    static final Rect MIDDLE_ROI = new Rect(
+            new Point(x:60, y:35),
+            new Point(x:120, y:75));
+    static final Rect RIGHT_ROI = new Rect(
+            new Point(x:60, y:35),
+            new Point(x:120, y:75));
+    public testopencv(Telemetry t) { telemetry = t; }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+        Scalar lowHSV = new Scalar(23, 50, 70);
+        Scalar highHSV = new Scalar(32, 255, 255);
+
+        Core.inRange(mat, lowHSV, highHSV, mat);
+        Mat left =
+    }*/
+}
