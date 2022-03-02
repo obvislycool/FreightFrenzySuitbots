c192618041a0f990740fc1879405c88455a79ac0
suitbot <suitbots@gmail.com>
2022-03-01 16:55:00 -0800

    asdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectorBlueStorage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectorBlueStorage.java
@@ -152,7 +152,7 @@ public class DetectorBlueStorage extends LinearOpMode {
         encoderDrive(TURN_SPEED, -8, 8, 2); //turn to hub
         encoderDrive(DRIVE_SPEED,  22,  22, 3);  //drive to hub
         encoderDrive(TURN_SPEED,   -44, +44, 4);  //180 turn
-        encoderDrive(0.35, -10.25, 10.25, 3);  //back into hub
+        encoderDrive(0.35, -10.25, -10.25, 3);  //back into hub
         //duckSpin(COUNTERCLOCKWISE,6000);
         //encoderDrive(DRIVE_SPEED,  -19,  19, 6.0);
         //encoderDrive(DRIVE_SPEED,  9,  9, 6.0);
@@ -183,7 +183,7 @@ public class DetectorBlueStorage extends LinearOpMode {
         encoderDrive(DRIVE_SPEED, 2, 2, 2);
         encoderDrive(TURN_SPEED, -3.5, 3.5, 1);
         encoderDrive(DRIVE_SPEED, 41, 41, 5);  // go to wheel
-        duckSpin(COUNTERCLOCKWISE,6000);
+        duckSpin(CLOCKWISE,6000);
         encoderDrive(TURN_SPEED,   -27, 27, 3);
         encoderDrive(DRIVE_SPEED,  19,  19, 3);
         //encoderDrive(DRIVE_SPEED,  -19,  19, 6.0);
